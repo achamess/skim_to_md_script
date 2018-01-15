96b653596eafd52a8ba7b2c01e5d9ebe69f17941
Alexander Chamessian <achamess@gmail.com>
2018-01-15 11:13:55 -0500

    changed get_papers script
    I modified the applescript
    - PDF is open in Skim.app
    - The file path of the PDF is gotten from Skim
    - That file path is then used to find the publication in Papers3
    - The metadata of the paper are then extracted from Papers3 into the
    skim_to_md.sh script

--- get_paper.sh
+++ get_paper.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env osascript 
-tell application "Papers"
-	set p to selected publications of front library window
-	set pathToFile to full path of primary file item of item 1 of p
-	set BibString to bibtex string of item 1 of p
-	set FullRef to formatted reference of item 1 of p
-	set ck to citekey of item 1 of p
-	set output to pathToFile & " + " & BibString & "+" & FullRef & "+" & ck
-	
+
+tell application "Skim"
+	set props to properties of document 1
+	set pathToFile to path of props
+	get pathToFile
 end tell
 
+tell application "Papers"
+	set pfi to every primary file item whose full path contains pathToFile
+	
+	if pfi is not equal to {} then
+		set p to properties of publication item of item 1 of pfi
+		set BibString to bibtex string of p
+		set FullRef to formatted reference of p
+		set ck to citekey of p
+		set output to pathToFile & " + " & BibString & "+" & FullRef & "+" & ck
+	else
+		display dialog "This paper is not in your library"
+	end if
+	
+end tell
 
